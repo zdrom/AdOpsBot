285f03a0cea31c553c89d1bddec73ce9e1f48153
ZDROM <zdr747@me.com>
2020-07-20 12:12:11 -0400

    Update Admin

--- creatives/admin.py
+++ creatives/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from.models import Creative
 
-admin.site.register(Creative)
+
+class CreativeAdmin(admin.ModelAdmin):
+    model = Creative
+    search_fields = ['adserver', 'blocking_vendor']
+    list_display = ('name', 'adserver', 'blocking_vendor', 'requested_by')
+
+
+admin.site.register(Creative, CreativeAdmin)
