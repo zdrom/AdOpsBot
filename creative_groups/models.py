a7f05fbfc5481167a44d98365ba096d576cb5523
ZDROM <zdr747@me.com>
2020-08-28 14:32:34 -0400

    Updates

--- creative_groups/models.py
+++ creative_groups/models.py
@@ -39,114 +39,118 @@ class CreativeGroup(models.Model):
 
     def click_and_pic(self, channel, progress_meter):
 
-        slack_client = WebClient(config('SLACK_BOT_TOKEN'))
+        try:
 
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        browser = webdriver.Chrome(options=chrome_options)
+            slack_client = WebClient(config('SLACK_BOT_TOKEN'))
 
-        creatives = self.creative_set.all()
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            browser = webdriver.Chrome(options=chrome_options)
 
-        p = 0  # progress meter counter
+            creatives = self.creative_set.all()
 
-        for creative in creatives:
+            p = 0  # progress meter counter
 
-            p += 1
+            for creative in creatives:
 
-            html_doc = creative.use_correct_markup()
+                p += 1
 
-            log.info(f'Mark Up with macros replaced: {creative.markup_with_macros_replaced}')
+                html_doc = creative.use_correct_markup()
 
-            browser.get("data:text/html;charset=utf-8,{html_doc}".format(html_doc=html_doc))
+                log.info(f'Mark Up with macros replaced: {creative.markup_with_macros_replaced}')
 
-            # Wait for the creative to render
-            browser.implicitly_wait(1.5)
+                browser.get("data:text/html;charset=utf-8,{html_doc}".format(html_doc=html_doc))
 
-            '''
-            Save the screenshot
-            Crop the image 
-            there is an 8x8 border on the top and left so crop include that in the crop
-            move from temp file to default storage
-            '''
-            temp = NamedTemporaryFile(prefix='screenshot', suffix='.png')
-            browser.save_screenshot(temp.name)
-            im = Image.open(temp)
-            cropped_dimensions = (8, 8, int(creative.width) + 8, int(creative.height) + 8)
-            log.warning(f'The cropped dimensions are {cropped_dimensions}')
-            cropped = im.crop(cropped_dimensions)
+                # Wait for the creative to render
+                browser.implicitly_wait(1.5)
 
-            temp_out = NamedTemporaryFile(prefix='screenshot_out', suffix='.png')
-            cropped.save(temp_out.name)
+                '''
+                Save the screenshot
+                Crop the image 
+                there is an 8x8 border on the top and left so crop include that in the crop
+                move from temp file to default storage
+                '''
+                temp = NamedTemporaryFile(prefix='screenshot', suffix='.png')
+                browser.save_screenshot(temp.name)
+                im = Image.open(temp)
+                cropped_dimensions = (8, 8, int(creative.width) + 8, int(creative.height) + 8)
+                log.warning(f'The cropped dimensions are {cropped_dimensions}')
+                cropped = im.crop(cropped_dimensions)
 
-            creative.screenshot = default_storage.save('screenshots/screenshot.png', temp_out)
-            creative.save()
+                temp_out = NamedTemporaryFile(prefix='screenshot_out', suffix='.png')
+                cropped.save(temp_out.name)
 
-            try:
-                if creative.adserver == 'dcm ins':
-                    el = browser.find_element_by_tag_name('ins')
-                elif creative.adserver == 'dcm legacy':
-                    el = browser.find_element_by_tag_name('a')
-                elif creative.adserver == 'sizmek':
-                    imgs = browser.find_elements_by_tag_name('img')
-                    # in case there are any 1x1s
-                    # find the image that has dimensions greater than 1x1
-                    for img in imgs:
-                        print(int(img.get_attribute('width')) > 1)
-                        if int(img.get_attribute('width')) > 1:
-                            el = img
-                            break
-                elif creative.adserver == 'flashtalking':
-                    # served in an iframe
-                    browser.switch_to.frame(0)
-                    # in case there are any 1x1s
-                    # find the image that has dimensions greater than 1x1
-                    imgs = browser.find_elements_by_tag_name('img')
-                    for img in imgs:
-                        if int(img.get_attribute('width')) > 1:
-                            el = img
-                            break
-
-                el.click()
-
-                # switch to the newly opened tab
+                creative.screenshot = default_storage.save('screenshots/screenshot.png', temp_out)
+                creative.save()
 
                 try:
-                    browser.switch_to.window(browser.window_handles[1])
-                except:
+                    if creative.adserver == 'dcm ins':
+                        el = browser.find_element_by_tag_name('ins')
+                    elif creative.adserver == 'dcm legacy':
+                        el = browser.find_element_by_tag_name('a')
+                    elif creative.adserver == 'sizmek':
+                        imgs = browser.find_elements_by_tag_name('img')
+                        # in case there are any 1x1s
+                        # find the image that has dimensions greater than 1x1
+                        for img in imgs:
+                            print(int(img.get_attribute('width')) > 1)
+                            if int(img.get_attribute('width')) > 1:
+                                el = img
+                                break
+                    elif creative.adserver == 'flashtalking':
+                        # served in an iframe
+                        browser.switch_to.frame(0)
+                        # in case there are any 1x1s
+                        # find the image that has dimensions greater than 1x1
+                        imgs = browser.find_elements_by_tag_name('img')
+                        for img in imgs:
+                            if int(img.get_attribute('width')) > 1:
+                                el = img
+                                break
+
+                    el.click()
+
+                    # switch to the newly opened tab
+
+                    try:
+                        browser.switch_to.window(browser.window_handles[1])
+                    except:
+                        log.exception(f'{creative.name} has an invalid click through')
+                        creative.click_through = 'Unable to determine'
+                        creative.save()
+                        slack_client.chat_update(channel=channel, ts=progress_meter['ts'], text=f'{ p } out of { creatives.count() } complete')
+                        continue
+
+                    '''
+                    Primarily for sizmek but this makes sure that an actual url is captured
+                    instead of about:blank
+                    , for instance
+                    '''
+
+                    log.info(f'Waiting for this to contain http: {browser.current_url}')
+
+                    WebDriverWait(browser, 10).until(
+                        EC.url_contains('http'))
+
+                    creative.click_through = browser.current_url
+
+                    log.info(f'{creative.name} click through: {creative.click_through}')
+                    creative.save()
+
+                    browser.close()
+                    browser.switch_to.window(browser.window_handles[-1])
+
+                    slack_client.chat_update(channel=channel, ts=progress_meter['ts'],
+                                             text=f'{p} out of {creatives.count()} complete')
+
+                except (NoSuchElementException, WebDriverException) as e:
                     log.exception(f'{creative.name} has an invalid click through')
                     creative.click_through = 'Unable to determine'
                     creative.save()
-                    slack_client.chat_update(channel=channel, ts=progress_meter['ts'], text=f'{ p } out of { creatives.count() } complete')
-                    continue
-
-                '''
-                Primarily for sizmek but this makes sure that an actual url is captured
-                instead of about:blank
-                , for instance
-                '''
-
-                log.info(f'Waiting for this to contain http: {browser.current_url}')
-
-                WebDriverWait(browser, 10).until(
-                    EC.url_contains('http'))
-
-                creative.click_through = browser.current_url
-
-                log.info(f'{creative.name} click through: {creative.click_through}')
-                creative.save()
-
-                browser.close()
-                browser.switch_to.window(browser.window_handles[-1])
-
-                slack_client.chat_update(channel=channel, ts=progress_meter['ts'],
-                                         text=f'{p} out of {creatives.count()} complete')
-
-            except (NoSuchElementException, WebDriverException) as e:
-                log.exception(f'{creative.name} has an invalid click through')
-                creative.click_through = 'Unable to determine'
-                creative.save()
-                slack_client.chat_update(channel=channel, ts=progress_meter['ts'],
-                                         text=f'{p} out of {creatives.count()} complete')
-
-        browser.quit()
+                    slack_client.chat_update(channel=channel, ts=progress_meter['ts'],
+                                             text=f'{p} out of {creatives.count()} complete')
+        except:
+            slack_client.chat_delete(channel=channel, ts=progress_meter['ts'])
+        finally:
+            browser.quit()
